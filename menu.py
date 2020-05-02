429db8ca11660eb72a2b0432ab3b25da7421f44c
JaesungLeee <viki0114@naver.com>
2020-05-02 17:56:46 +0900

    mix helpscreen with menu

--- menu.py
+++ menu.py
@@ -1,13 +1,10 @@
 import pygame, sys
-import pygame.freetype
-from pygame.sprite import Sprite
 from pygame.rect import Rect
-import helpScreen, sound
+import sound
 
 
 width, height = 1024, 876
 WHITE = (255, 255, 255)
-GRAY = (100, 100, 100)
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Metalslug")
@@ -45,7 +42,7 @@ def play(screen):
                     pass
             if help_rect.collidepoint(pygame.mouse.get_pos()):
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    helpScreen.play(screen)
+                    help_screen(screen)
             if close_rect.collidepoint(pygame.mouse.get_pos()):
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     pygame.quit()
@@ -55,5 +52,31 @@ def play(screen):
                 pygame.quit()
                 sys.exit()
             
+def help_screen(screen):
+    pygame.init()
+
+    backscreen = pygame.image.load("resources/images/helpscreen.png")
+    backscreen = pygame.transform.scale(backscreen, (1024, 876))
+    
+    screen.blit(backscreen, (0, 0))
+    
+    font = pygame.font.SysFont("resources/font/나눔손글씨붓.ttf", 65)
+    goback_button = font.render("Go Back", True, WHITE)
+    goback_rect = goback_button.get_rect(x = 100, y = 700)
+    screen.blit(goback_button, goback_rect)
+
+    pygame.display.flip()
+    
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if goback_rect.collidepoint(pygame.mouse.get_pos()):
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    play(screen)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+
 if __name__ == "__main__":
     play(screen)
