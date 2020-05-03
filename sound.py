0dc5face6b1ded41ce77bfce64840df5aff7c02e
JaesungLeee <viki0114@naver.com>
2020-05-03 17:20:29 +0900

    update

--- sound.py
+++ sound.py
@@ -1,24 +1,14 @@
 import pygame
 import time
-from pygame import *
 
 def intro_music(sound):
-    pygame.init()
-    pygame.mixer.init()
-    clock = pygame.time.Clock()
     pygame.mixer.music.load("resources/sound/intro_music.mp3")
     pygame.mixer.music.play(-1)
 
 def complete_music(sound):
-    pygame.init()
-    pygame.mixer.init()
-    clock = pygame.time.Clock()
     pygame.mixer.music.load("resources/sound/victory.ogg")
     pygame.mixer.music.play(-1)
 
 def gameover_music(sound):
-    pygame.init()
-    pygame.mixer.init()
-    clock = pygame.time.Clock()
     pygame.mixer.music.load("resources/sound/GameOver.ogg")
     pygame.mixer.music.play(-1)
